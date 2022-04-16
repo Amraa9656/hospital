31d065184978e5514136386fd296348670e1440a
amarjargalamraa61 <amarjargalamraa61@gmail.com>
2022-04-16 15:27:41 +0800

    main

--- components/navbar/Navbar.js
+++ components/navbar/Navbar.js
@@ -1,7 +1,30 @@
+import React from "react";
+import Nav from 'react-bootstrap/Nav'
+
 function Navbar() {
     return (
     <>
-        <h1>H</h1>
+       
+       <Nav className="col-md-12 d-none d-md-block bg-light sidebar"
+            activeKey="/home"
+            onSelect={selectedKey => alert(`selected ${selectedKey}`)}
+            >
+                <div className="sidebar-sticky"></div>
+            <Nav.Item>
+                <Nav.Link href="/home">Active</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+                <Nav.Link eventKey="link-1">Link</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+                <Nav.Link eventKey="link-2">Link</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+                <Nav.Link eventKey="disabled" disabled>
+                Disabled
+                </Nav.Link>
+            </Nav.Item>
+            </Nav>
     </> );
 }
 
